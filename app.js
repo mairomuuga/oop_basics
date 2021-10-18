5ac7669455c88e62425d4c4a9414f53561a13a74
mairomuuga <mairo.muuga@khk.ee>
2021-10-18 08:48:45 +0300

    OOJS 1,2 and 3 done

---
+++ app.js
@@ -0,0 +1,24 @@
+class Shape{
+	constructor(name, sides, sideLength){
+		this.name = name;
+		this.sides = sides;
+		this.sideLength = sideLength;
+	}
+	calcPerimeter(){
+		return this.sides * this.sideLength;
+	}
+}
+class Square extends Shape{
+	constructor(sideLength){
+		super("square" ,4 ,sideLength);
+	}
+	calcArea(){
+		return this.sideLength * this.sideLength;
+	}
+}
+let square = new Square(5);
+
+console.log(square);
+console.log(square.calcPerimeter());
+console.log(square.calcArea());
+
